86f4ce3e8e769d94730d652f5b2733b11a65c339
kuwabara yuki <yuki.kuwabara@drecom.co.jp>
2015-12-08 20:38:02 +0900

    indent

--- Sandbox/cpp/CppSandbox.h
+++ Sandbox/cpp/CppSandbox.h
@@ -2,7 +2,7 @@
 
 extern "C"
 {
-	jint JNI_OnLoad(JavaVM *vm, void *reserved);
+    jint JNI_OnLoad(JavaVM *vm, void *reserved);
 
     void Java_jp_do_1low_bazel_sample_AppActivity_jniCallback(JNIEnv* env, jclass cls, jstring val);
 }
